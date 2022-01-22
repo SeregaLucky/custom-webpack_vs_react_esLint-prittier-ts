e29fe3a2bb459a2919cad445b3b38c6383cb43be
SeregaLucky <strizgsergey@gmail.com>
2022-01-22 02:09:47 +0200

    +

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-console.log(process);
-console.log(process.env);
-console.log(process.env.time);
-console.log(process.env.DEVELOPMENT);
-console.log(process.env.PRODUCTION);
-console.log(process.env.URL_M);
+// console.log(process);
+// console.log(process.env);
+// console.log(process.env.time);
+// console.log(process.env.DEVELOPMENT);
+// console.log(process.env.PRODUCTION);
+// console.log(process.env.URL_M);
 
 render(
   <StrictMode>
